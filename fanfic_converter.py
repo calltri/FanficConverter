860d397224401149191fc35da6a40b65d9e898ca
Madison W. Call <madison.call122@gmail.com>
2023-06-29 12:09:39 -0700

    Removed code to parse google doc formatting. Replaced with requirement to use the docs to markdown add on to convert code to a actually usable format. Reformatted partitioning code and tests to reflect this

--- fanfic_converter.py
+++ fanfic_converter.py
@@ -1,4 +1,6 @@
 # Converts html code from a google doc into code compatible with different platforms
+# NOTE: Use "Docs to Markdown" add on to convert text to html. Then copy/paste
+# that into a document 
 
 import sys
 import os
@@ -13,50 +15,7 @@ def read_html_file(file_name):
     f.close()
     return html
 
-def convert_strikes_to_ao3(fanfic_code):
-    # Converts code to AO3 format
-    # input: HTML text
-    # returns: new HTML text
-    ao3_code = fanfic_code
-
-    # TODO google doc line is text-decoration:line-through
-    # need to find that and then the c# associated with it
-
-    #TODO error
-    # dfjdkfjskjfksl </span><span class="c1">even more useless
-    # Types of endings:
-    # </span><span>&nbsp;ignorant</span><span>. 
-    # </span><span>&nbsp;had a particularly good relationship</span><span>.
-    # </span><span>&nbsp;disappeared. 
-    # </span><span class="c2">&nbsp;her
-    # </span><span class="c2"> - lacks $nbsp;
-
-    ao3_code = ao3_code.replace("&nbsp;", " ")
-    while(True):
-        try:
-            span_index = ao3_code.index(STRIKE_FORMATTING)
-            span_index = ao3_code.find("</span>", span_index)
-            #find the end of the next span
-            last_index = ao3_code.find(">", span_index + len("</span><span")) + 2
-            print(ao3_code[span_index:last_index])
-            #remove docs c4 formatting and replace with simple <strike> formatting
-            ao3_code = ao3_code[:span_index] + "</strike> " + ao3_code[last_index:]
-            
-            ao3_code = ao3_code.replace("</span>" + STRIKE_FORMATTING, "<strike>", 1)
-        except ValueError:
-            break
-    return ao3_code
-
-def isolate_header(fanfic_code):
-    # isolates the header and body code from each other
-    last_index = fanfic_code.find("</head>")
-    header = fanfic_code[len("<html>"):last_index + len("</head>")]
-    # shrink fanfic code to pure text
-    fanfic_code = fanfic_code[last_index + len("</head>"):-(len("</html>"))]
-    return header, fanfic_code
-
-
-def partition_html_into_chapters(header, fanfic_code):
+def partition_html_into_chapters(fanfic_code):
     # NOTE: DIVIDES BY h3 HEADER
     # partition code into several chapters
     # input: html text
@@ -69,11 +28,11 @@ def partition_html_into_chapters(header, fanfic_code):
             # check if another chapter exists
             next_index = fanfic_code.find(CHAPTER_TITLE_FORMATTING, first_index + 1)
             if next_index != -1:
-                fanfic_sections.append("<html>" + header + "<body>" + fanfic_code[first_index:next_index] + "</body></html>")
+                fanfic_sections.append(fanfic_code[first_index:next_index])
                 fanfic_code = fanfic_code[next_index:]
             else:
                 # Body end tag already exists
-                fanfic_sections.append("<html>" + header + "<body>" + fanfic_code[first_index:] + "</html>")
+                fanfic_sections.append(fanfic_code[first_index:])
                 break
         except ValueError:
             print("ERROR: No h3 header exists")
@@ -92,9 +51,7 @@ def main():
     fanfic_code = read_html_file(file)
     file_name = file[len(".\\doc_fanfics\\\\"):-len(".html")]
     
-    fanfic_code = convert_strikes_to_ao3(fanfic_code)
-    header, fanfic_code = isolate_header(fanfic_code)
-    fanfic_sections = partition_html_into_chapters(header, fanfic_code)
+    fanfic_sections = partition_html_into_chapters(fanfic_code)
     iterator = 0
     for section in fanfic_sections:
         f = open(".\\output_fanfics\\" + file_name + "_" + str(iterator) + ".html", "w")
--- test_fanfics.py
+++ test_fanfics.py
@@ -7,52 +7,36 @@ FULL_HEADER_NAME = "test_fanfics\header.txt"
 FULL_ENTIRE_BODY_NAME = "test_fanfics\entire_body.txt"
 FULL_NUM_SECTIONS = 9
 
-PARTITION_HEADER = "<head></head>"
-BODY_START = "<body>"
-BODY_END = "</body>"
-PARTITION_BODY_START = "<html>" + PARTITION_HEADER + "<body>"
-PARTITION_BODY_END = "</body></html>"
 PARTITIONING_TEST_DATA = [
     {"test_name": "ch and p", "body_code":"<h3>chapter</h3><p></p>", "num_sections":1, 
-        "sections": [PARTITION_BODY_START + "<h3>chapter</h3><p></p>" + PARTITION_BODY_END]},
+        "sections": ["<h3>chapter</h3><p></p>"]},
     {"test_name": "ch", "body_code":"<h3>chapter</h3>", "num_sections":1, 
-        "sections": [PARTITION_BODY_START + "<h3>chapter</h3>" + PARTITION_BODY_END]},
+        "sections": ["<h3>chapter</h3>"]},
     {"test_name": "p", "body_code":"<p></p>", "num_sections":0, 
         "sections": []},
     {"test_name": "p and ch", "body_code":"<p></p><h3>chapter</h3>", "num_sections":1, 
-        "sections": [PARTITION_BODY_START + "<h3>chapter</h3>" + PARTITION_BODY_END]},
+        "sections": ["<h3>chapter</h3>"]},
     {"test_name": "ch p and ch", "body_code":"<h3>chapter</h3><p></p><h3>chapter</h3>", "num_sections":2, 
-        "sections": [PARTITION_BODY_START + "<h3>chapter</h3><p></p>" + PARTITION_BODY_END,
-                     PARTITION_BODY_START + "<h3>chapter</h3>" + PARTITION_BODY_END]},
+        "sections": ["<h3>chapter</h3><p></p>",
+                     "<h3>chapter</h3>"]},
     {"test_name": "ch p ch and p", "body_code":"<h3>chapter</h3><p></p><h3>chapter</h3><p></p>", "num_sections":2, 
-        "sections": [PARTITION_BODY_START + "<h3>chapter</h3><p></p>" + PARTITION_BODY_END,
-                     PARTITION_BODY_START + "<h3>chapter</h3><p></p>" + PARTITION_BODY_END]},
+        "sections": ["<h3>chapter</h3><p></p>",
+                     "<h3>chapter</h3><p></p>"]},
     {"test_name": "null test", "body_code":"", "num_sections":0, 
         "sections": []},
 ]
 
 class TestClass:
 
-    def test_full_isolate_header(self):
-        fanfic_code = fanfic_converter.read_html_file(FULL_TEST_FILE_NAME)
-        header, fanfic_code = fanfic_converter.isolate_header(fanfic_code)
-        assert len(header) > 0
-        assert header == fanfic_converter.read_html_file(FULL_HEADER_NAME)
-        assert fanfic_code == fanfic_converter.read_html_file(FULL_ENTIRE_BODY_NAME)
-
     def test_partitioning(self):
         for test_data in PARTITIONING_TEST_DATA:
             print("Starting test: " + test_data["test_name"])
-            sections = fanfic_converter.partition_html_into_chapters(PARTITION_HEADER, 
-                                                                     BODY_START + 
-                                                                     test_data["body_code"] + 
-                                                                     BODY_END)
+            sections = fanfic_converter.partition_html_into_chapters(test_data["body_code"])
             assert len(sections) == test_data["num_sections"]
             assert sections == test_data["sections"]
             print("Passed test: " + test_data["test_name"])
-            
+
     def test_full_partition_html_into_sections(self):
         fanfic_code = fanfic_converter.read_html_file(FULL_TEST_FILE_NAME)
-        header, fanfic_code = fanfic_converter.isolate_header(fanfic_code)
-        sections = fanfic_converter.partition_html_into_chapters(header, fanfic_code)
+        sections = fanfic_converter.partition_html_into_chapters(fanfic_code)
         assert len(sections) == FULL_NUM_SECTIONS
